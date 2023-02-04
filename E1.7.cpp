97f92c1d43f2a2eb4383989a90ea8c83f326152d
jameschen2004 <jameschen200465@gmail.com>
2023-02-03 19:51:54 -0500

    finished E1.7

---
+++ E1.7.cpp
@@ -0,0 +1,18 @@
+/*
+Author: James
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: E1.7 - Write a program that prints three items, such as the names of your three best friends
+or favorite movies, on three separate lines.
+
+*/
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "apple \n";
+    std::cout << "banana \n";
+    std::cout << "orange" << std::endl;
+    return 0;
+}
