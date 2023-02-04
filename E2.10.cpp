d06fec532bacd69db25c2ba547df460d59946400
jameschen2004 <jameschen200465@gmail.com>
2023-02-03 21:21:14 -0500

    finished E2.10

---
+++ E2.10.cpp
@@ -0,0 +1,34 @@
+/*
+Author: James
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: E2.10 - Write a program that asks the user to input
+• The number of gallons of gas in the tank
+• The fuel efficiency in miles per gallon
+• The price of gas per gallon
+Then print the cost per 100 miles and how far the car can go with the gas in the tank.
+
+*/
+
+#include <iostream>
+
+int main(){
+
+    int gas_in_tank;
+    int fuel_efficiency;
+    int price_per_gal;
+    int cost_per_100_miles;
+    int possible_miles_travelled;
+
+    std::cin >> gas_in_tank;
+    std::cin >> fuel_efficiency;
+    std::cin >> price_per_gal;
+
+    cost_per_100_miles = (100 / fuel_efficiency) * price_per_gal;
+    std::cout << cost_per_100_miles << std::endl;
+    
+    possible_miles_travelled = gas_in_tank * fuel_efficiency;
+    std::cout << possible_miles_travelled << std::endl;
+
+    return 0;
+}
