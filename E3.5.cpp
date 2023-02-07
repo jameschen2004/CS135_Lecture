86db3f7a3d446e28db7f0a72f18b88b2272156d7
jameschen2004 <jameschen200465@gmail.com>
2023-02-07 13:03:27 -0500

    Finished E3.5

--- E3.5.cpp
+++ E3.5.cpp
@@ -6,7 +6,6 @@ Assignment: E3.5 - Write a program that reads three numbers and prints
 “increasing” if they are in increasing order, “decreasing” if they are 
 in decreasing order, and “neither” otherwise. Here, “increasing” means 
 “strictly increasing”, with each value larger than its predecessor. 
-The sequence 3 4 4 would not be considered increasing.
 
 */
 
